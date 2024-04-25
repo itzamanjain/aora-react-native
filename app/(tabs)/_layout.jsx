f0b5ef2cea1c3c2477711813a19567189bca0896
itzamanjain <aman.044063@tmu.ac.in>
2024-04-26 00:24:02 +0530

    added assests

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text ,Image} from 'react-native'
 import React from 'react'
+import { Tabs,Redirect } from 'expo-router'
+
+const TabIcon = () =>{
+  return (
+    <View>
+      <Image />
+    </View>
+  )
+}
 
 const TabLayout = () => {
   return (
-    <View>
-      <Text>TabLayout</Text>
-    </View>
+    <>
+      <Tabs>
+        <Tabs.Screen name="home" />
+      </Tabs>
+    </>
   )
 }
 
